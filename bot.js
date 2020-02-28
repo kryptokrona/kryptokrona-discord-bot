fc56b0f9da8837e1871df8fd7a08bba29c5c8712
Jensengbg-Carl-Ulvinen <54705232+Jensengbg-Carl-Ulvinen@users.noreply.github.com>
2020-02-28 14:38:28 +0100

    Update bot.js
    Added commands for the bot

--- bot.js
+++ bot.js
@@ -160,6 +160,13 @@ client.on('message', msg => {
           .addField("!register <address>", 'Registers a kryptokrona address for receiving tips (tags shouldn\'t be used)', false )
           .addField("!tip <@user> <amount>", 'Sends <amount> XKR to <@user> (tags shouldn\'t be used)', false )
           .addField("!send <address> <amount>", 'Sends <amount> XKR to <address> (tags shouldn\'t be used)', false )
+          //addendum
+          .addField("!tipall <amount>", 'Sends <amount> XKR to all registered wallets (tags shouldn\'t be used)', false )
+          .addField("!tiprandom <amount>", 'Sends <amount> XKR to random registered wallet (tags shouldn\'t be used)', false )
+
+          // is the !balance command missing?
+          //.addField("!balance", 'display wallet balance', false )
+          
         // Send the embed to the same channel as the message
 
 
